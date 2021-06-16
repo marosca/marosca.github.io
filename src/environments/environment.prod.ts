d73dbce46a231f7769dda0d2e39aa3e91a0dd3c6
Marosca <maria.moreno@masmovil.com>
2021-06-16 19:27:02 +0200

    remove unnecesary media from app src

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
+
 export const environment = {
-  production: true
+   production: true,
+   SPACE_ID: "ujjv6x66ivgi",
+   ACCESS_TOKEN: "CFPAT-aVy-IPJkQZUMAD1BXMvHVhXXYikphMXd4osqxNQ9RU4",
+   CDN_CONTENTFUL: "cdn.contentful.com",
+   CDN_PREVIEW: "preview.contentful.com",
+   CONTENT_ACCESS_TOKEN: "yTkgvAY_Jx4BnEgo9la217_87x7_pSMhCc4ctm8WVw0",
+   CONTENT_PREVIEW_ACCESS_TOKEN: "DV8IyLpl4o7gU3HLB6YfGzjX8IiYBO-wWCWvOqcEVxo"
 };
